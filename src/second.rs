ec7a67d183ba4743bb7132759a5cfd36f0e16da2
James Abley <james.abley@gmail.com>
2020-08-04 20:55:42 +0100

    Remove explicit lifetimes
    We can apply lifetime elision here.

--- src/second.rs
+++ src/second.rs
@@ -77,10 +77,7 @@ pub struct Iter<'a, T> {
 
 // No lifetime here, List doesn't have any associated lifetimes
 impl<T> List<T> {
-    // We declare a fresh lifetime here for the *exact* borrow that
-    // creates the iter. Now &self needs to be valid as long as the
-    // Iter is around.
-    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+    pub fn iter(&self) -> Iter<T> {
         Iter {
             next: self.head.as_ref().map::<&Node<T>, _>(|node| &node),
         }
