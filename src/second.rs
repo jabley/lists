9a1837673af41640251e57eb1b0fbc8d48788525
James Abley <james.abley@gmail.com>
2020-08-04 21:00:02 +0100

    Add draft for IterMut
    Copy the `Iter` code and change everything to be mutable.
    
    Needs work.
    
      error[E0596]: cannot borrow `self.head` as mutable, as it is behind a `&` reference
         --> src/second.rs:108:19
          |
      106 |     pub fn iter_mut(&self) -> IterMut<'_, T> {
          |                     ----- help: consider changing this to be a mutable reference: `&mut self`
      107 |         IterMut {
      108 |             next: self.head.as_mut().map(|node| &mut **node),
          |                   ^^^^^^^^^ `self` is a `&` reference, so the data it refers to cannot be borrowed as mutable
    
      error[E0507]: cannot move out of `self.next` which is behind a mutable reference
         --> src/second.rs:117:9
          |
      117 |         self.next.map(|node| {
          |         ^^^^^^^^^
          |         |
          |         move occurs because `self.next` has type `std::option::Option<&mut second::Node<T>>`, which does not implement the `Copy` trait
          |         help: consider borrowing the `Option`'s content: `self.next.as_ref()`

--- src/second.rs
+++ src/second.rs
@@ -98,6 +98,29 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter_mut(&self) -> IterMut<'_, T> {
+        IterMut {
+            next: self.head.as_mut().map(|node| &mut **node),
+        }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
