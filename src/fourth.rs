1f3bd3742f54beb0e2e34d86810db440c2f52f76
James Abley <james.abley@gmail.com>
2020-08-05 17:19:23 +0100

    Try to add push_front
    This fails
    
      error[E0609]: no field `prev` on type `std::rc::Rc<std::cell::RefCell<fourth::Node<T>>>`
        --> src/fourth.rs:41:26
         |
      41 |                 old_head.prev = Some(new_head.clone()); // +1 new_head
         |                          ^^^^ unknown field
    
      error[E0609]: no field `next` on type `std::rc::Rc<std::cell::RefCell<fourth::Node<T>>>`
        --> src/fourth.rs:42:26
         |
      42 |                 new_head.next = Some(old_head); // +1 old_head
         |                          ^^^^ unknown field

--- src/fourth.rs
+++ src/fourth.rs
@@ -31,4 +31,24 @@ impl<T> List<T> {
             tail: None,
         }
     }
+
+    pub fn push_front(&mut self, elem: T) {
+        // new node needs +2 links, everything else should be +0
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                // non-empty list, need to connect the old_head
+                old_head.prev = Some(new_head.clone()); // +1 new_head
+                new_head.next = Some(old_head); // +1 old_head
+                self.head = Some(new_head); // +1 new_head, -1 old_head
+                                            // total: +2 new_head, +0 old_head -- OK!
+            }
+            None => {
+                // empty list, need to set the tail
+                self.tail = Some(new_head.clone()); // +1 new_head
+                self.head = Some(new_head); // +1 new_head
+                                            // total: +2 new_head -- OK!
+            }
+        }
+    }
 }
