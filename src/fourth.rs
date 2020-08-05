d9ee20d7112751a1af1bfe1290d009f3d9ebce6c
James Abley <james.abley@gmail.com>
2020-08-05 17:43:41 +0100

    Try to copy what we did for Drop earlier
    `try_unwrap` worked before. Will it work in this context?
    
    No.
    
      error[E0599]: no method named `unwrap` found for enum `std::result::Result<std::cell::RefCell<fourth::Node<T>>, std::rc::Rc<std::cell::RefCell<fourth::Node<T>>>>` in the current scope
         --> src/fourth.rs:73:38
          |
      73  |             Rc::try_unwrap(old_head).unwrap().into_inner().elem
          |                                      ^^^^^^ method not found in `std::result::Result<std::cell::RefCell<fourth::Node<T>>, std::rc::Rc<std::cell::RefCell<fourth::Node<T>>>>`
          |
         ::: /Users/james.abley/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src/liballoc/rc.rs:285:1
          |
      285 | pub struct Rc<T: ?Sized> {
          | ------------------------ doesn't satisfy `_: std::fmt::Debug`
          |
          = note: the method `unwrap` exists but the following trait bounds were not satisfied:
                  `std::rc::Rc<std::cell::RefCell<fourth::Node<T>>>: std::fmt::Debug`

--- src/fourth.rs
+++ src/fourth.rs
@@ -70,7 +70,7 @@ impl<T> List<T> {
                                       // total: -2 old, (no new)
                 }
             }
-            old_head.into_inner().elem
+            Rc::try_unwrap(old_head).unwrap().into_inner().elem
         })
     }
 }
