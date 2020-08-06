2d725b65a4ff941d64d435079681821f46904c4a
James Abley <james.abley@gmail.com>
2020-08-06 20:21:14 +0100

    Fix the use of moved value by using a reference
      error[E0106]: missing lifetime specifier
       --> src/fifth.rs:3:18
        |
      3 |     tail: Option<&mut Node<T>>, // NEW!
        |                  ^ expected named lifetime parameter
        |
      help: consider introducing a named lifetime parameter
        |
      1 | pub struct List<'a, T> {
      2 |     head: Link<T>,
      3 |     tail: Option<&'a mut Node<T>>, // NEW!
        |

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,8 +1,6 @@
-use std::mem;
-
 pub struct List<T> {
     head: Link<T>,
-    tail: Link<T>, // NEW!
+    tail: Option<&mut Node<T>>, // NEW!
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -27,18 +25,20 @@ impl<T> List<T> {
             next: None,
         });
 
-        // swap the old tail to point to the new tail
-        let old_tail = mem::replace(&mut self.tail, Some(new_tail));
-
-        match old_tail {
-            Some(mut old_tail) => {
+        // Put the box in the right place, and then grab a reference to its Node
+        let new_tail = match self.tail.take() {
+            Some(old_tail) => {
                 // If the old tail existed, update it to point to the new tail
                 old_tail.next = Some(new_tail);
+                old_tail.next.as_mut().map(|node| &mut **node)
             }
             None => {
                 // Otherwise, update the head to point to it
                 self.head = Some(new_tail);
+                self.head.as_mut().map(|node| &mut **node)
             }
-        }
+        };
+
+        self.tail = new_tail;
     }
 }
