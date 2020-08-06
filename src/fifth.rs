406bce9a2715a1ecfab10670bf701abbc6939e3f
James Abley <james.abley@gmail.com>
2020-08-06 20:44:40 +0100

    Add pop

--- src/fifth.rs
+++ src/fifth.rs
@@ -38,4 +38,15 @@ impl<T> List<T> {
         }
         self.tail = raw_tail;
     }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|head| {
+            let head = *head;
+            self.head = head.next;
+            if self.head.is_none() {
+                self.tail = ptr::null_mut();
+            }
+            head.elem
+        })
+    }
 }
