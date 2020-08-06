2abd698de55c818e7a34d60fe93a34e91e9f18ea
James Abley <james.abley@gmail.com>
2020-08-06 20:39:44 +0100

    Add a constructor for the new queue

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,3 +1,5 @@
+use std::ptr;
+
 pub struct List<T> {
     head: Link<T>,
     tail: *mut Node<T>, // DANGER DANGER
@@ -8,4 +10,13 @@ type Link<T> = Option<Box<Node<T>>>;
 struct Node<T> {
     elem: T,
     next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: ptr::null_mut(),
+        }
+    }
 }
